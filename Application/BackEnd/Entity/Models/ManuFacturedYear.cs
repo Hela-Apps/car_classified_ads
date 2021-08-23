92c8625c4c346bd34d75178de3514a61b4321156
Chamath Viduranga <chamath@bistecglobal.com>
2021-08-23 08:14:08 +0530

    master tables

---
+++ Application/BackEnd/Entity/Models/ManuFacturedYear.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entity.Models
+{
+    public class ManuFacturedYear
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Year { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
